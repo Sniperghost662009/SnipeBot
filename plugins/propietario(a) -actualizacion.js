87297d6bea2a7adee850b4b583303fbcbfc2eeb6
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2023-11-25 20:01:07 -0600

    Update propietario(a) -actualizacion.js

--- plugins/propietario(a) -actualizacion.js
+++ plugins/propietario(a) -actualizacion.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
-const owner = 'GataNina-Li';
-const repo = 'GataBot-MD';
+const owner = 'Snipe';
+const repo = 'SnipeBot';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   async function checkRepoUpdates() {
     try {
