e9082cc2fe0f42b63752ce4461f411eff1fc18ee
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-01 11:49:39 -0600

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -251,6 +251,8 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}  
+
+/* 
 import fs from 'fs'
 import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
@@ -374,6 +376,11 @@ buttonText: `${lenguajeGB['smsListaMenu']()}`,
 sections }
 await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})
 
+
+} catch (e) {
+await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)	
 }}
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|m|\?)$/i
 //handler.register = true
@@ -407,3 +414,4 @@ function ucapan() {
   }
   return res
 } 
+*/
