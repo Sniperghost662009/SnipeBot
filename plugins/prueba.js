4dc8374af420834133f16013a5ffc8320c490e42
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-01 11:24:08 -0600

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -12,53 +12,22 @@ let msg = generateWAMessageFromContent(m.chat, {
         },
         interactiveMessage: proto.Message.InteractiveMessage.create({
           body: proto.Message.InteractiveMessage.Body.create({
-            text: "test"
+            text: "Menu"
           }),
           footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "test"
+            text: "Menu"
           }),
           header: proto.Message.InteractiveMessage.Header.create({
-            title: "test",
-            subtitle: "test",
+            title: "Menu",
+            subtitle: "Menu",
             hasMediaAttachment: false
           }),
           nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
             buttons: [
               {
                 "name": "single_select",
-                "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"quick_reply\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "cta_url",
-                 "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
-              },
-              {
-                 "name": "cta_call",
-                 "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "cta_copy",
-                 "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
-              },
-              {
-                 "name": "cta_reminder",
-                 "buttonParamsJson": "{\"display_text\":\"cta_reminder\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "cta_cancel_reminder",
-                 "buttonParamsJson": "{\"display_text\":\"cta_cancel_reminder\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "address_message",
-                 "buttonParamsJson": "{\"display_text\":\"address_message\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "send_location",
-                 "buttonParamsJson": ""
+                "buttonParamsJson": "{\"restringir , restrict, descripcion: bot.restrict ? Activado || Activated ❌ Desactivado || Disabled'Permisos para el Bot\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+          
               }
            ],
           })
